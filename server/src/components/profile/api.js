46734925bd45b49b1ce5a7774a6d1240f53abcf6
unknown <christianlugod05@gmail.com>
2020-11-15 18:03:39 +0800

    server fix

--- server/src/components/profile/api.js
+++ server/src/components/profile/api.js
@@ -3,9 +3,9 @@ const { auth } = require("../../middleware");
 const router = express.Router();
 const profileController = require("./controller");
 
-router.get("/get/:username", auth, profileController.getProfile);
-router.get("/get/about/:username", auth, profileController.getProfileAbout);
-router.get("/get/post/:username", auth, profileController.getProfilePost);
+router.get("/get/:username", profileController.getProfile);
+router.get("/get/about/:username", profileController.getProfileAbout);
+router.get("/get/post/:username", profileController.getProfilePost);
 router.put("/update/profile-photo", auth, profileController.updateProfilePhoto);
 router.put("/update/cover-photo", auth, profileController.updateCoverPhoto);
 router.put("/remove/profile-photo", auth, profileController.removeProfilePhoto);
